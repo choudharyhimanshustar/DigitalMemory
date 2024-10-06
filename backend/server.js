f1fbfce3d29827e84e3306116f3dbdaa81d6cf67
Choudhary Himanshu <himanshuch3003@gmail.com>
2024-10-06 18:18:45 +0530

    Updated CORS Policy

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ const connect = require('./connection/db');
 const cors = require('cors');
 require('dotenv').config();
 const signUp = require('./SignUp')
-const Login=require('./Login');
+const Login = require('./Login');
 const app = express();
-const Home=require('./Home')
+const Home = require('./Home')
 connect();
 app.use(cors({
-    origin: 'https://digital-memory-one.vercel.app',  // Only allow requests from this origin
+    origin: 'https://digital-memory-one.vercel.app',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
 }));
 app.use(express.json());
-app.use('/',Home);
+app.use('/', Home);
 app.use('/signUp', signUp);
 app.use('/login', Login);
 const PORT = process.env.PORT || 2000;
