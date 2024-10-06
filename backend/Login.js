0d9fed9135314993190647730d9e046880166c41
Choudhary Himanshu <himanshuch3003@gmail.com>
2024-10-06 17:33:06 +0530

    Backend

---
+++ backend/Login.js
@@ -0,0 +1,25 @@
+const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
+const express = require('express');
+const router = express.Router();
+const User=require('./models/Users')
+router.post('/',async(req,res)=>{
+    const {email,password}=req.body;
+    try
+    {
+        const user=await User.findOne({email});
+       
+        if(!user)
+            return res.json({error:"User does not exist"});
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch)
+            return res.json({error:"Invalid Password"});
+        const token=jwt.sign({id:user._id},process.env.Secret_Key);
+        return res.json({token});
+    }
+    catch(err)
+    {
+        res.json({err});
+    }
+})
+module.exports=router;
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connect = require('./connection/db');
+const cors = require('cors');
+require('dotenv').config();
+const signUp = require('./SignUp')
+const Login=require('./Login');
+const app = express();
+const Home=require('./Home')
+connect();
+app.use(cors({
+    origin: 'http://localhost:3000',  // Only allow requests from this origin
+}));
+app.use(express.json());
+app.use('/',Home);
+app.use('/signUp', signUp);
+app.use('/login', Login);
+const PORT = process.env.PORT || 2000;
+app.listen(PORT, () => { console.log(`Server connected on ${PORT}`) });
