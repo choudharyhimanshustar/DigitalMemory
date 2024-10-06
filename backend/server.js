bc80fd64cbc1c2162d473ffa46f51b80bd661772
Choudhary Himanshu <himanshuch3003@gmail.com>
2024-10-06 18:10:23 +0530

    Commit

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const Home=require('./Home')
 connect();
 app.use(cors({
-    origin: 'https://digital-memory-one.vercel.app/',  // Only allow requests from this origin
+    origin: 'https://digital-memory-one.vercel.app',  // Only allow requests from this origin
 }));
 app.use(express.json());
 app.use('/',Home);
