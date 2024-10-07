7d57fc6faeb618036368a286bf3160627b094648
Choudhary Himanshu <himanshuch3003@gmail.com>
2024-10-07 15:33:57 +0530

    CORS

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,13 @@ app.use(cors({
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
 }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://digital-memory-one.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 app.options('*', cors());
 app.use(express.json());
 app.use('/', Home);
