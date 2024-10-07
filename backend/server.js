529194ab6e30ee6677bf22b58ad1b1e66123c32f
Choudhary Himanshu <himanshuch3003@gmail.com>
2024-10-07 18:17:54 +0530

    Server.js

--- backend/server.js
+++ backend/server.js
@@ -7,23 +7,20 @@ const Login = require('./Login');
 const app = express();
 const Home = require('./Home')
 connect();
-
+const allowedOrigins = [
+    'https://digital-memory-one.vercel.app',
+    'http://localhost:3000'  // Allow local requests
+];
 app.use(cors({
-    origin: 'https://digital-memory-one.vercel.app',
+    origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
 }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://digital-memory-one.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
+
 app.options('*', cors());
 app.use(express.json());
 app.use('/', Home);
 app.use('/signUp', signUp);
 app.use('/login', Login);
-const PORT = process.env.PORT || 2000;
+const PORT = 2000;
 app.listen(PORT, () => { console.log(`Server connected on ${PORT}`) });
