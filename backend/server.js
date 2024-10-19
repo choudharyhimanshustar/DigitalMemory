832bb4b81886025dc535f674c20b9c841e74740f
Choudhary Himanshu <himanshuch3003@gmail.com>
2024-10-19 07:24:38 +0530

    Backend

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,13 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://digital-memory-one.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
-app.options('*', cors());
+app.options('*', cors({
+  origin: "https://digital-memory-one.vercel.app",
+  credentials: true
+}));
 app.use('/', Home);
 app.use('/signUp', signUp);
 app.use('/login', Login);
